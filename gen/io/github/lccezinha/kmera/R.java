dabbff5a8933c5e3118a1b38b7075c5e323a5466
Luiz Cezer <lccezinha@gmail.com>
2015-08-31 21:40:43 -0300

    Ticaracaticas pt2

--- gen/io/github/lccezinha/kmera/R.java
+++ gen/io/github/lccezinha/kmera/R.java
@@ -15,7 +15,8 @@ public final class R {
     }
     public static final class id {
         public static final int camera=0x7f070000;
-        public static final int media_player=0x7f070001;
+        public static final int mediaPlayer=0x7f070001;
+        public static final int videoView=0x7f070002;
     }
     public static final class layout {
         public static final int camera=0x7f030000;
@@ -24,6 +25,7 @@ public final class R {
     }
     public static final class raw {
         public static final int megadeth=0x7f040000;
+        public static final int videord=0x7f040001;
     }
     public static final class string {
         public static final int app_name=0x7f050004;
