81be43db72fb105c4640c3bf9f43b738cb943e5d
Luiz Cezer <lccezinha@gmail.com>
2015-09-01 22:33:22 -0300

    Add mapa

--- gen/io/github/lccezinha/kmera/R.java
+++ gen/io/github/lccezinha/kmera/R.java
@@ -14,13 +14,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int camera=0x7f070003;
+        public static final int camera=0x7f070004;
         public static final int latitude=0x7f070001;
-        public static final int location=0x7f070005;
+        public static final int location=0x7f070006;
         public static final int longitude=0x7f070002;
-        public static final int mediaPlayer=0x7f070004;
+        public static final int map=0x7f070003;
+        public static final int mediaPlayer=0x7f070005;
         public static final int provider=0x7f070000;
-        public static final int videoView=0x7f070006;
+        public static final int videoView=0x7f070007;
     }
     public static final class layout {
         public static final int camera=0x7f030000;
--- src/io/github/lccezinha/kmera/activities/LocationActivity.java
+++ src/io/github/lccezinha/kmera/activities/LocationActivity.java
@@ -7,12 +7,17 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class LocationActivity extends Activity {
 	private LocationManager locationManager;
 	private TextView provider, longitude, latitude;
-	
+	private WebView map;
+	private String urlBase = "http://maps.googleapis.com/maps/api" +
+		"/staticmap?size=400x400&sensor=true" +
+		" &markers=color:red|%s,%s";
+		
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -43,10 +48,14 @@ public class LocationActivity extends Activity {
 					String.valueOf(location.getLatitude());
 			String longitudeStr =
 					String.valueOf(location.getLongitude());
+			String url = 
+					String.format(urlBase, latitudeStr, longitudeStr);
 			
 			provider.setText(location.getProvider());
 			latitude.setText(latitudeStr);
 			longitude.setText(longitudeStr);
+			
+			map.loadUrl(url);
 		}
 
 		@Override
@@ -63,5 +72,6 @@ public class LocationActivity extends Activity {
 		provider = (TextView) findViewById(R.id.provider);
 		latitude = (TextView) findViewById(R.id.latitude);
 		longitude = (TextView) findViewById(R.id.longitude);
+		map = (WebView) findViewById(R.id.map);
 	}
 }
